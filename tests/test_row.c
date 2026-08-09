38c09295949829b9ee252af208d0feb5f093d88b
Paula Meuronen <paula.meuronen@helsinki.fi>
2026-08-09 21:50:42 +0300

    Row tests

--- tests/test_row.c
+++ tests/test_row.c
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "../src/common.h"
-#include "../src/col.h"
+#include "../src/row.h"
 #include "../src/checker.h"
 
 int8_t check_line(void){
     return 0; // mock
 }
 
-void test_extract_column(void){
+void test_extract_row(void){
     board_width = 3;
     memset(gameboard, 0, sizeof(gameboard)); //Initialize game board
 
@@ -24,27 +24,26 @@ void test_extract_column(void){
 
     memcpy(gameboard, board, sizeof(board));
 
-    int8_t result = extract_column(1);
-
-    assert(line[0] == 1);
+    int8_t result = extract_row(1);
+    assert(line[0] == 0);
     assert(line[1] == 0);
-    assert(line[2] == 2);
+    assert(line[2] == 0);
     assert(result == 0);
 
 }
 
-void test_extract_column_invalid_indices(void){
+void test_extract_row_invalid_indices(void){
     board_width = 3;
-    assert(extract_column(-1) == -1);
-    assert(extract_column(3) == -1);
-    assert(extract_column(10) == -1);
+    assert(extract_row(-1) == -1);
+    assert(extract_row(3) == -1);
+    assert(extract_row(10) == -1);
 }
 
 int main(void) {
-    test_extract_column();
-    printf("Test to extract column passed!\n");
-    test_extract_column_invalid_indices();
-    printf("Test to check invalid column values passed!\n");
+    test_extract_row();
+    printf("Test to extract row passed!\n");
+    test_extract_row_invalid_indices();
+    printf("Test to check invalid row values passed!\n");
     printf("All tests passed!\n");
     return 0;
 }
