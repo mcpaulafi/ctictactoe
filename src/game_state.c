c72e7b8a9fe67e4e77e37f156ea3ddb081e795c2
Paula Meuronen <paula.meuronen@helsinki.fi>
2026-08-10 20:44:02 +0300

    Removed unnecessary breaks

--- src/game_state.c
+++ src/game_state.c
@@ -16,16 +16,13 @@ int8_t process_state(int8_t status){
         case 0:
             printf("Settings for a new TicTacToe-game.\n");
             return set_game();
-            break;
         case 1:
             if(debug) {printf("Print board and input cell\n");}
             print_gameboard();
             return input_cell();
-            break;
         case 2:
             if(debug) {printf("Update board\n");}
             return update_board();
-            break;
         case 3:
             if(debug) {printf("Check result\n");} 
             int8_t result = extract_lines();
@@ -44,7 +41,6 @@ int8_t process_state(int8_t status){
             }
             print_gameboard();
             return 7;
-            break;
         case 5:
             printf("No win yet. Continue.\n\n");
             if (player == 1) { //Switch player
@@ -53,22 +49,18 @@ int8_t process_state(int8_t status){
                 player = 1;
             }
             return 1;
-            break;
         case 6:
             printf("TIE! Game over.\n");
             print_gameboard();
             return 7;
-            break;
         case 7:
             if(debug) {printf("New game?\n");}
             int8_t start_new = new();
              if (start_new == 1){
                 return 0;
-                break;
             }else{
                 printf("End program.\n");
                 return -1;
-                break;
             }
         default:
             printf("Error\n");
